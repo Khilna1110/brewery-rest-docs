f2a799b5115729afa0ecc35a5e5f57a73e4597f8
Khilna Gudhka <khilnahgudhka@gmail.com>
2025-08-04 15:29:03 +0530

    adding snake case example

---
+++ src/test/java/guru/springframework/msscbrewery/web/model/BeerDtoSnakeTest.java
@@ -0,0 +1,28 @@
+package guru.springframework.msscbrewery.web.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("snake")
+@JsonTest
+class BeerDtoSnakeTest extends BaseTest{
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void testSnakeDto() throws JsonProcessingException {
+
+        BeerDto beerDto=getDto();
+
+        String json=objectMapper.writeValueAsString(beerDto);
+
+        System.out.println(json);
+
+
+    }
+}
